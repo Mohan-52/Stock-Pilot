07ff1f163498ef9127a7e0aaf309cc76ffdae463
mohan-52 <mohanaoruganti52@gmail.com>
2026-03-09 22:19:05 +0530

    base entity and auditorAwareImpl completed

--- backend/src/main/java/com/mohan/stock_pilot/StockPilotApplication.java
+++ backend/src/main/java/com/mohan/stock_pilot/StockPilotApplication.java
@@ -2,8 +2,10 @@ package com.mohan.stock_pilot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef= "auditorAwareImpl")
 public class StockPilotApplication {
 
 	public static void main(String[] args) {
