ae517b30a32af5d8a23396a95e97ed4d4eb833ff
mohan-52 <mohanaoruganti52@gmail.com>
2026-03-09 21:29:56 +0530

    first commit

---
+++ backend/src/main/java/com/mohan/stock_pilot/StockPilotApplication.java
@@ -0,0 +1,13 @@
+package com.mohan.stock_pilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPilotApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mohan/stock_pilot/StockPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohan.stock_pilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
